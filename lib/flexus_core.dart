e85ce1aa870a161564c48b9c195ffaf2a5aba75f
Dilan <dilan@dilan.me>
2022-03-20 14:48:14 +0530

    [builder] Releasing v1.0.6

--- lib/flexus_core.dart
+++ lib/flexus_core.dart
@@ -1,7 +1,17 @@
 library flexus_core;
 
+import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
+
 /// A Calculator.
 class FlexusCore {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+  FlexusCore._privateConstructor();
+  static final FlexusCore _instance = FlexusCore._privateConstructor();
+  static FlexusCore get instance => _instance;
+
+  Widget getApp(Widget app) {
+    return Sizer(builder: (context, orientation, deviceType) {
+      return app;
+    });
+  }
 }
