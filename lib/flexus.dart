5823cae8a98138b00f33db6399e6a77763bf4443
Dilan <dilan@dilan.me>
2022-04-28 17:37:46 +0530

    [builder] Releasing v1.0.18

---
+++ lib/flexus.dart
@@ -0,0 +1,30 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:logger/logger.dart';
+import 'package:sizer/sizer.dart';
+
+class Fc {
+  late Logger log;
+  Fc._privateConstructor();
+  static final Fc _instance = Fc._privateConstructor();
+  static Fc get instance => _instance;
+
+  init(Logger log) {
+    this.log = log;
+  }
+
+  Widget getApp(Widget app, {bool devicePreview = false}) {
+    if (devicePreview) {
+      return DevicePreview(
+          enabled: devicePreview,
+          builder: (context) =>
+              Sizer(builder: (context, orientation, deviceType) {
+                return app;
+              }));
+    } else {
+      return Sizer(builder: (context, orientation, deviceType) {
+        return app;
+      });
+    }
+  }
+}
--- lib/flexus_core.dart
+++ lib/flexus_core.dart
@@ -1,33 +1,14 @@
 library flexus_core;
 
-import 'package:device_preview/device_preview.dart';
-import 'package:flutter/material.dart';
 import 'package:logger/logger.dart';
-import 'package:sizer/sizer.dart';
 
-/// A Calculator.
-class Fc {
-  late Logger log;
-  Fc._privateConstructor();
-  static final Fc _instance = Fc._privateConstructor();
-  static Fc get instance => _instance;
+import 'flexus.dart';
 
+class FlexusCore {
+  FlexusCore._privateConstructor();
+  static final FlexusCore _instance = FlexusCore._privateConstructor();
+  static FlexusCore get instance => _instance;
   init(Logger log) {
-    this.log = log;
-  }
-
-  Widget getApp(Widget app, {bool devicePreview = false}) {
-    if (devicePreview) {
-      return DevicePreview(
-          enabled: devicePreview,
-          builder: (context) =>
-              Sizer(builder: (context, orientation, deviceType) {
-                return app;
-              }));
-    } else {
-      return Sizer(builder: (context, orientation, deviceType) {
-        return app;
-      });
-    }
+    Fc.instance.init(log);
   }
 }
